b3b31e37e9ea398b7625db9203992e866a54df62
OlZe <oliverzeidler97@gmail.com>
2026-03-17 22:57:49 +0100

    european datetime formatting

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,14 +5,14 @@ import {
   isDevMode,
 } from '@angular/core';
 import { provideRouter, withHashLocation, withViewTransitions } from '@angular/router';
-
+import { provideLuxonDateAdapter } from '@angular/material-luxon-adapter';
 import { routes } from './app.routes';
 import { withNgxsReduxDevtoolsPlugin } from '@ngxs/devtools-plugin';
 import { withNgxsStoragePlugin } from '@ngxs/storage-plugin';
 import { provideStore } from '@ngxs/store';
 import { ExpensesState } from './state/expenses/expenses.state';
 import { HomePage } from './views/home-page/home-page';
-import { provideNativeDateAdapter } from '@angular/material/core';
+import { MAT_DATE_LOCALE, provideNativeDateAdapter } from '@angular/material/core';
 import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
@@ -24,7 +24,8 @@ export const appConfig: ApplicationConfig = {
       withNgxsStoragePlugin({ keys: '*' }),
       withNgxsReduxDevtoolsPlugin(),
     ),
-    provideNativeDateAdapter(),
+    provideLuxonDateAdapter(),
+    { provide: MAT_DATE_LOCALE, useValue: 'de-DE' },
     provideServiceWorker('ngsw-worker.js', {
       enabled: !isDevMode(),
       registrationStrategy: 'registerWhenStable:30000',
